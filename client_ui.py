f83552234fa9c5342d5c09cc2f7ac8e9899e70af
findj <findj@>
2017-05-06 20:58:14 -0700

    Signed-off-by: findj <findj@>

---
+++ client_ui.py
@@ -0,0 +1,16 @@
+'''
+Created on May 6, 2017
+
+@author: findj
+'''
+import Tkinter as tk
+
+def render(frame):
+    button = tk.Button(frame, )
+
+# main function
+if __name__ == "__main__":
+    frame = tk.Tk()
+    
+    
+
