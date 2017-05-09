bfff0dbc05e4256d3ea6b62b3933d41bc0b2bb83
findj <findj@192.168.1.67>
2017-05-08 20:25:58 -0700

    minior change

--- client_ui.py
+++ client_ui.py
@@ -6,11 +6,14 @@ Created on May 6, 2017
 import Tkinter as tk
 
 def render(frame):
-    button = tk.Button(frame, )
+#     button = tk.Button(frame, )
+    frame.mainloop()
+    return
 
 # main function
 if __name__ == "__main__":
     frame = tk.Tk()
+    render(frame)
     
     
 
